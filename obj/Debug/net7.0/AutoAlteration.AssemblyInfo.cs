f199ef08515a28f382e2c8b29662b9e48e8b62c6
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2024-06-03 12:39:05 +0200

    Blockmove filename

--- obj/Debug/net7.0/AutoAlteration.AssemblyInfo.cs
+++ obj/Debug/net7.0/AutoAlteration.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoAlteration")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e938fd6aaef727c0ee82be8acb8eb0218263ed0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+551c7b75ef51cba64861d69afc87a517ae9425f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoAlteration")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoAlteration")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
