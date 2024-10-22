f35776b5df5c4169a6e70fb5e2d3895035801db0
TobiasgutPrivat <79203201+TobiasgutPrivat@users.noreply.github.com>
2024-10-22 18:02:31 +0200

    SnowScenery Skins

--- src/Alterations/SceneryAlterations.cs
+++ src/Alterations/SceneryAlterations.cs
@@ -30,5 +30,6 @@ class SnowScenery : Alteration
         // map.map.Blocks.ToList().Where(x => x.Skin != null).Where(x => x.Skin.Text == "PlatformIce\\").ToList().ForEach(x => x.Skin.Save("C:/Users/Tobias/Documents/Programmieren/AutoAlteration/IceSkin.gbx"));
         CGameCtnBlockSkin GrassSkin = Gbx.Parse<CGameCtnBlockSkin>("C:/Users/Tobias/Documents/Programmieren/AutoAlteration/data/IceSkin.gbx");
         map.map.Blocks.ToList().ForEach(x => x.Skin = GrassSkin);
+        map.map.BakedBlocks.ToList().ForEach(x => x.Skin = GrassSkin);
     }
 }
